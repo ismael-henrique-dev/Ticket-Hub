497aec9d166c35584679788739ec67b4d656628e
Ismael <ismael.henrique.dev@gmail.com>
2025-02-09 13:25:32 -0300

    feat: add select filter data in transports

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const setors = [
+  { value: 'Air', label: 'Aéreo' },
+  { value: 'Land', label: 'Terrestre' },
+  { value: 'Naval', label: 'Marítimo' },
+  { value: 'Rail', label: 'Ferroviário' },
+]
